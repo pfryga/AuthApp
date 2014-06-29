3c6173e0902f52bd7c0b16fe735c05be28429a49
Piotr Fryga <piotr.fryga@allegro.pl>
2014-06-29 17:12:49 +0200

    connect with facebookAPP

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var FACEBOOK_APP_ID = 'facebookID';
+var FACEBOOK_APP_SECRET = 'facebookSecret';
+
 var app = express();
+
+ 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,10 +25,50 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+passport.use(new FacebookStrategy({
+  clientID: FACEBOOK_APP_ID,
+  clientSecret: FACEBOOK_APP_SECRET,
+  callbackURL: 'http://localhost:3000/auth/facebook/callback'
+}, function(accessToken, refreshToken, profile, done) {
+  process.nextTick(function() {
+    //Assuming user exists
+    done(null, profile);
+  });
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+ 
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/success',
+  failureRedirect: '/error'
+}));
+ 
+app.get('/success', function(req, res, next) {
+  res.send('Successfully logged in.');
+});
+ 
+app.get('/error', function(req, res, next) {
+  res.send("Error logging in.");
+});
+ 
+app.get('/', function(req, res, next) {
+  res.sendfile('./html/auth.html');
+});
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
